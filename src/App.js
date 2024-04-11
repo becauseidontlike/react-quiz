14433b9c1310dc5253938622d110bc226e9da71f
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-11 22:32:41 +0200

    start of quiz app, new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import DateCounter from "./DateCounter";
+import Header from "./Header";
 
 export default function App() {
   return (
-    <div className="">
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <main className="main">
+        <p>1/15</p>
+        <p>questions</p>
+      </main>
     </div>
   );
 }
