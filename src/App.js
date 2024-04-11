cf9cc942433e580348302938c72289f383a94c74
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-11 22:45:23 +0200

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
+
   return (
     <div className="app">
       <Header />
