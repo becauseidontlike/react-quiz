36012e851f45ed5cdbac052f3671876f6e1e9842
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-17 22:09:12 +0200

    emojis :astonished:

--- src/components/Finished.js
+++ src/components/Finished.js
@@ -1,10 +1,16 @@
 function Finished({ points, maxPossiblePoints }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎖️";
+  if (percentage >= 50 && percentage < 80) emoji = "🫡";
+  if (percentage < 50) emoji = "😐";
+
   return (
     <p className="result">
       You scored <strong>{points}</strong> out of {maxPossiblePoints} (
-      {Math.ceil(percentage)}%)
+      {Math.ceil(percentage)}%) <span>{emoji}</span>
     </p>
   );
 }
